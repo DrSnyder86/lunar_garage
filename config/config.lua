5224117d12598a8b6904df1b3d45f8da3b8f3324
DrSnyder <drsnyder86@gmail.com>
2026-04-25 15:57:32 -0400

    Enable AutoRespawn for vehicles on script restart
    Add AutoRespawn configuration for vehicle management.

--- config/config.lua
+++ config/config.lua
@@ -2,7 +2,7 @@ Config = {}
 Config.MaxDistance = 10.0 -- Max interact distance
 Config.UseKeySystem = true -- Implemented only for qb-vehiclekeys, you can implement it for other systems in cl_edit.lua
 Config.SpawnpointCheck = true -- Checks if the vehicle spawnpoint is empty before spawning it.
-
+Config.AutoRespawn = true -- True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
 -- The global setting for target however you can still combine target/TextUI by omitting Position or PedPosition in garage/impound data
 Config.Target = true
 
